6d15070e499e9893bcca2c28c82d31359b51570a
Kishor Kc <kishor8007kc@gmail.com>
2021-06-28 14:10:33 +0545

    Initial Commit

--- lib/res/appwriteConstant.dart
+++ lib/res/appwriteConstant.dart
@@ -1,4 +1,4 @@
 class AppwriteConstant {
-  static const String projectId = "60d4b687e0336";
-  static const String endpontId = "https://192.168.1.10/v1";
+  static const String projectId = "use your project ID";
+  static const String endpontId = "use your end point";
 }
