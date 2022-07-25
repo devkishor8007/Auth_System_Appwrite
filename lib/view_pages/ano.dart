1d5e960b9cc6755dc13adb4b90128a286b1a8e8d
kekavc24 <kelvinkavisi707@gmail.com>
2022-07-25 12:42:28 +0300

    Fixed errors

--- lib/view_pages/ano.dart
+++ lib/view_pages/ano.dart
@@ -1,20 +1,28 @@
 import 'package:appwrite/appwrite.dart';
-import 'package:auth_system/data/service/appwriteAUTH_service.dart';
+import 'package:auth_system/data/service/appwrite_auth_service.dart';
 import 'package:auth_system/main.dart';
-import 'package:auth_system/widget/makeText.dart';
+import 'package:auth_system/widget/make_text.dart';
 import 'package:auth_system/widget/route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class AnoPage extends StatelessWidget {
+class AnoPage extends StatefulWidget {
+  const AnoPage({Key? key}) : super(key: key);
+
+  @override
+  State<AnoPage> createState() => _AnoPageState();
+}
+
+class _AnoPageState extends State<AnoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Anonymous User"),
+        title: const Text("Anonymous User"),
       ),
       drawer: Drawer(
         child: ListView(
-          padding: EdgeInsets.all(0),
+          padding: const EdgeInsets.all(0),
           children: [
             UserAccountsDrawerHeader(
               accountName: makeText(
@@ -27,19 +35,22 @@ class AnoPage extends StatelessWidget {
                   "A",
                   textColor: Colors.black,
                   fontWeight: FontWeight.bold,
-                  size: Theme.of(context).textTheme.headline6.fontSize,
+                  size: Theme.of(context).textTheme.headline6!.fontSize!,
                 ),
               ),
             ),
             ListTile(
               title: makeText("Logout"),
-              leading: Icon(Icons.logout),
+              leading: const Icon(Icons.logout),
               onTap: () async {
                 try {
-                  await AuthAppwrite.instance.logout();
-                  pushReplacement(context, MainPage());
+                  await AuthAppwrite.instance!.logout();
+
+                  if (!mounted) return;
+                  pushReplacement(context, const MainPage());
+
                 } on AppwriteException catch (e) {
-                  print(e.message);
+                  if (kDebugMode) print(e.message);
                 }
               },
             ),
@@ -47,14 +58,14 @@ class AnoPage extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             makeText(
               "Forget all the reasons it won’t work and believe the one reason that it will!",
               fontWeight: FontWeight.bold,
-              size: Theme.of(context).textTheme.headline6.fontSize,
+              size: Theme.of(context).textTheme.headline6!.fontSize!,
             ),
             makeText("Anonymous Quotes"),
           ],
